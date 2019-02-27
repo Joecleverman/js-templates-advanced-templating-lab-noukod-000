1ad6b96b2e24947b6e0ded93f618b4cda929b9d0
Josue Abraham <josuea@noukod.com>
2019-02-27 18:20:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,21 +2,21 @@ function initForm() {
   var formTemplate = document.getElementById("recipe-form-template").innerHTML
   var template = Handlebars.compile(formTemplate)
   document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
-};
+}
 
 function createRecipe() {
   var recipe = getRecipeVals()
   var recipeTemplate = document.getElementById("recipe-template").innerHTML
   var template = Handlebars.compile(recipeTemplate)
   document.getElementById("main").innerHTML = template(recipe)
-};
+}
 
 function updateRecipe() {
   var recipe = getRecipeVals()
   var recipeTemplate = document.getElementById("recipe-template").innerHTML
   var template = Handlebars.compile(recipeTemplate)
   document.getElementById("main").innerHTML = template(recipe)
-};
+}
 
 function displayEditForm() {
   var name = document.getElementById("nameHeader").innerText
@@ -25,7 +25,7 @@ function displayEditForm() {
   var ingredients = []
   for(var i=0;i<ingredientsNodes.length;i++) {
     ingredients.push(ingredientsNodes[i].innerText)
-  };
+  }
 
   var recipe = {name, description, ingredients, submitAction: 'createRecipe()'}
 
@@ -40,8 +40,8 @@ function getRecipeVals() {
   for(var i=0;i<ingredientsNodes.length;i++) {
     if(ingredientsNodes[i].value !== "") {
       ingredients.push(ingredientsNodes[i].value)
-    };
-  };
+    }
+  }
   var name = document.getElementById("name").value
   var description = document.getElementById("description").value
   var recipe = {name, ingredients, description}
@@ -55,14 +55,14 @@ function handlebarsSetup() {
   });
   Handlebars.registerPartial('recipeDetailsPartial', document.getElementById("recipe-details-partial").innerHTML)
   Handlebars.registerPartial('recipeFormPartial', document.getElementById("recipe-form-partial").innerHTML)
-};
+}
 
 
 function init() {
   //put any page initialization/handlebars initialization here
   handlebarsSetup()
   initForm()
-};
+}
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
 });
